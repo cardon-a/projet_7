6d60bb755e966deb7f2aab7b5612315df68ce07f
cardon-a <alexis.cardon@hotmail.fr>
2023-02-13 18:11:33 +0100

    Ajout de la redirection vers /404

--- src/pages/Lodging/Lodging.jsx
+++ src/pages/Lodging/Lodging.jsx
@@ -1,6 +1,6 @@
 import './Lodging.css';
 import LodgingList from '../../data/lodging.json'
-import { useParams } from 'react-router-dom';
+import { useParams, Navigate } from 'react-router-dom';
 import Star from '../../assets/pages/Lodging/star.svg'
 import StarEmpty from '../../assets/pages/Lodging/starEmpty.svg'
 import Tag from '../../components/Tag/Tag'
@@ -13,7 +13,7 @@ function Lodging() {
 
   let rating = []   // Ajout des étoiles pour la note
   for (let i = 0; i < 5; i++) {
-    if (i < parseInt(lodging.rating)) {
+    if (i < parseInt(lodging?.rating)) {
       rating.push(<img key={ i } src={ Star } alt='Étoile pleine'></img>)
     } else {
       rating.push(<img key={ i } src={ StarEmpty } alt='Étoile vide'></img>)
@@ -21,32 +21,38 @@ function Lodging() {
   }
 
   let equipmentDescription = ''
-  lodging.equipments.map(equipment => equipmentDescription = equipmentDescription + equipment + '\n')
+  lodging?.equipments.map(equipment => equipmentDescription = equipmentDescription + equipment + '\n')
   
   return (
-    <div className="lodging">
-      <Carousel images={ lodging.pictures }/>
-      <div className='lodging__infos'>
-        <div className='lodging__infos__left'>
-          <h1>{ lodging.title }</h1>
-          <span>{ lodging.location }</span>
-          <Tag tags={ lodging.tags }/>
-        </div>
-        <div className='lodging__infos__right'>
-          <div className='lodging__infos__right__owner'>
-            <span>{ lodging.host.name }</span>
-            <img src={ lodging.host.picture } alt='Propriétaire'/>
+    <>
+      {
+        lodging ? (
+          <div className="lodging">
+            <Carousel images={ lodging.pictures }/>
+            <div className='lodging__infos'>
+              <div className='lodging__infos__left'>
+                <h1>{ lodging.title }</h1>
+                <span>{ lodging.location }</span>
+                <Tag tags={ lodging.tags }/>
+              </div>
+              <div className='lodging__infos__right'>
+                <div className='lodging__infos__right__owner'>
+                  <span>{ lodging.host.name }</span>
+                  <img src={ lodging.host.picture } alt='Propriétaire'/>
+                </div>
+                <div className='lodging__infos__right__rate'>
+                  { rating }
+                </div>
+              </div>
+            </div>
+            <div className='lodging__collapse'>
+              <Collapse title='Description' description={ lodging.description }/>
+              <Collapse title='Équipements' description={ equipmentDescription }/>
+            </div>
           </div>
-          <div className='lodging__infos__right__rate'>
-            { rating }
-          </div>
-        </div>
-      </div>
-      <div className='lodging__collapse'>
-        <Collapse title='Description' description={ lodging.description }/>
-        <Collapse title='Équipements' description={ equipmentDescription }/>
-      </div>
-    </div>
+        ) : <Navigate replace to='/404'/>
+      }
+    </>
   );
 }
 
